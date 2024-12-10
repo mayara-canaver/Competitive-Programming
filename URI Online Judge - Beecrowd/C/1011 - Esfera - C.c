337fa815863755d9c22ffe4e64fdcd9f3684d041
mayara-canaver <mayaracanaver@hotmail.com>
2024-12-09 23:57:19 -0300

    restructuring folders and files

--- URI Online Judge - Beecrowd/C/1011 - Esfera - C.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- 
-int main() {
- double raio, pi=3.14159, volume;
-	scanf("%lf", &raio);
-	volume = (4.0/3)*pi*(raio*raio*raio);
-	printf("VOLUME = %.3lf\n", volume);
-	return 0;
-}
--- URI Online Judge - Beecrowd/C/2950 - As Duas Torres - C.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	float resp,N,X,Y;
-	scanf("%f %f %f", &N,&X,&Y);
-	resp = N/(X+Y);
-	printf("%.2f\n",resp);
-}
